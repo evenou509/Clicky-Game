7e99592be2670d39b8c00ebdea648d3ccb631da8
Jean Fils-Aime <jeaneddy25@gmail.com>
2019-01-17 20:13:43 -0500

    Updates

---
+++ precache-manifest.1301ec72c4b4d855bc0b8121a3da8c82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "d2df6ba4cdca08355672",
+    "url": "/Clicky-Game/static/js/main.d2df6ba4.chunk.js"
+  },
+  {
+    "revision": "ab826ed5304424716772",
+    "url": "/Clicky-Game/static/js/1.ab826ed5.chunk.js"
+  },
+  {
+    "revision": "d2df6ba4cdca08355672",
+    "url": "/Clicky-Game/static/css/main.c40b8fa2.chunk.css"
+  },
+  {
+    "revision": "2bc2cbf183745962e3e1d4c2ed0cadf6",
+    "url": "/Clicky-Game/index.html"
+  }
+];
